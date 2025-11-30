2209b1386275263becff30c592fafffa5d6f28a5
AkkaRin11 <artemcrepper@gmail.com>
2025-11-30 17:20:04 +0300

    init

---
+++ src/main/java/ru/akkarin/is_lab2/IsLab2Application.java
@@ -0,0 +1,13 @@
+package ru.akkarin.is_lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/akkarin/is_lab2/IsLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akkarin.is_lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
